304bae491b7896d6ce53dce9c9218a6764e7829e
Adam Peterson <adpete44@gmail.com>
2018-10-08 15:28:43 -0600

    Basic methods - Dispaly in Stack Controller

--- DataStructuresAssignment/Controllers/StackController.cs
+++ DataStructuresAssignment/Controllers/StackController.cs
@@ -8,10 +8,63 @@ namespace DataStructuresAssignment.Controllers
 {
     public class StackController : Controller
     {
+
+        Stack<string> myStack = new Stack<string>();
+
         // GET: Stack
         public ActionResult Index()
         {
             return View();
+        }
+
+        public void addOne()
+        {
+            myStack.Push("New Entry " + (myStack.Count + 1));
+            Index();
+        }
+
+        public void addHuge()
+        {
+            for (int i = 0; i < 2001; i++)
+            {
+                myStack.Push("New Entry " + (myStack.Count + 1));
+            }
+        }
+
+        /*add display method*/
+
+        public void deleteFromStack()
+        {
+            myStack.Pop();
+        }
+
+        public void clearStack()
+        {
+            myStack.Clear();
+        }
+
+        public void searchStack()
+        {
+            bool bFound;
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+            sw.Start();
+
+            if (myStack.Contains("2"))
+            {
+                bFound = true;
+            }
+            else
+            {
+                bFound = false;
+            }
+
+            sw.Stop();
+
+            TimeSpan ts = sw.Elapsed;
+
+            ViewBag.StopWatch = ts;
+            ViewBag.StopWatch += bFound;
         }
     }
 }
