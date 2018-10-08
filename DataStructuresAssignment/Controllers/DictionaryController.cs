fd2c0952ca8caf76fb0b789f7a9fe99b72cd94bd
Adam Peterson <adpete44@gmail.com>
2018-10-08 14:52:29 -0600

    Added basic files

---
+++ DataStructuresAssignment/Controllers/DictionaryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataStructuresAssignment.Controllers
+{
+    public class DictionaryController : Controller
+    {
+        // GET: Dictionary
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DataStructuresAssignment/Controllers/ExitController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataStructuresAssignment.Controllers
+{
+    public class ExitController : Controller
+    {
+        // GET: Exit
+        public ActionResult Index()
+        {
+            return new RedirectResult("https://www.byu.edu");
+        }
+    }
+}
